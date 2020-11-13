8b7b75135be26cc0f22328a614296f2a9a3ef556
Hamid Yaftian <hamid.yaftian@gmail.com>
2020-11-13 16:30:37 +0330

    fix vueitfy global variables file address in nuxt.config.js

--- nuxt.config.js
+++ nuxt.config.js
@@ -66,7 +66,7 @@ export default {
 
     // Vuetify module configuration (https://go.nuxtjs.dev/config-vuetify)
     vuetify: {
-        customVariables: ['~/assets/variables.scss'],
+        customVariables: ['~/assets/scss/variables.scss'],
         theme: {
             dark: false,
             themes: {
